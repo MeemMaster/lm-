cd20befeb1cbd8ec25f1ba0d3499f8285f1e5a82
MeemMaster <76259517+MeemMaster@users.noreply.github.com>
2021-02-26 20:36:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var plyer 
+
+
+
+
+function preload(){
+
+}
+function setup(){       
+createCanvas(600,660) 
+player=createSprite(200,200,100, 100  )
+} 
+function draw(){          
+background("red")
+drawSprites()
+}
